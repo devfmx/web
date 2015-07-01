169d08a4c7d23e0d9b2b9b387a6af13b0b5946d7
Pablo Angel Trinidad Paz <pabloatp0204@gmail.com>
2015-07-01 17:05:10 -0500

    Hacker Definitions

--- static/js/devf.js
+++ static/js/devf.js
@@ -1,5 +1,22 @@
-// animations
-TweenMax.from('.devf-center-vh', 1,{
-	y: 100,
-	ease: Elastic.easeOut
+$(function () {
+
+	// Init Scroll Magic
+	var scrollMagicController = new ScrollMagic();
+
+	TweenMax.from('.devf-center-vh', 0.8,{
+		y: 100,
+		ease: Elastic.easeOut
+	});
+
+	var animateHDTexts = TweenMax.staggerFrom('.devf-hd-text', 0.5, {
+		x: '999',
+		ease: Power2.easeIn
+	});
+
+	var scene = new ScrollScene({
+	    triggerElement: '#devf-hacker-definitions',
+	    offset: 1
+	})
+	.setTween(animateHDTexts)
+	.addTo(scrollMagicController);
 });
