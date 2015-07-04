36ab8e33124c728c7a07a34cf9106482601c5dcb
Pablo Angel Trinidad Paz <pabloatp0204@gmail.com>
2015-07-04 18:17:59 -0500

    Programs section

--- programs/models.py
+++ programs/models.py
@@ -1,20 +1,36 @@
+"""Programs Model."""
+
 from django.db import models
 from django_markdown.models import MarkdownField
 
+
 class ProgramQuerySet(models.QuerySet):
-	def published(self):
-		return self.filter(publish=True)
+
+    """Class to add custom querysets to Program Model."""
+
+    def published(self):
+        """Get only the published programs."""
+        return self.filter(publish=True)
+
 
 class Program(models.Model):
-	name = models.CharField(max_length=255)
-	slug = models.SlugField(max_length=200, unique=True)
-	publish = models.BooleanField(default=False)
-	created = models.DateTimeField(auto_now_add=True)
-	modified = models.DateTimeField(auto_now=True)
-	short_description = models.CharField(max_length=1300)
-	description = MarkdownField()
 
-	objects = ProgramQuerySet.as_manager()
+    """Porgram Model."""
 
-	def __unicode__(self):
-		return self.name
+    name = models.CharField(max_length=255)
+    old_price = models.FloatField(default=0.0)
+    actual_price = models.FloatField(default=0.0)
+    requirements = models.TextField(default='', null=True)
+    logo = models.ImageField(upload_to='program_images', null=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    publish = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    short_description = models.CharField(max_length=1300)
+    description = MarkdownField()
+
+    objects = ProgramQuerySet.as_manager()
+
+    def __unicode__(self):
+        """Return the name when instance the class."""
+        return self.name
