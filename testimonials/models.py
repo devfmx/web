2edc52effd62ea6fdcc525f5cc753963c7507212
Pablo Angel Trinidad Paz <pabloatp0204@gmail.com>
2015-07-05 01:01:09 -0500

    Press model

--- testimonials/models.py
+++ testimonials/models.py
@@ -19,8 +19,34 @@ class Testimonial(models.Model):
 
     def ViewPicture(self):
         """Return a HTML with the testimonial person picture."""
-        return """<img src="%s" style="
-                  display: block; width: 50px;"/>
-               """ % self.picture.url
+        if self.picture.url:
+            return """<img src="%s" style="
+                      display: block; width: 50px;"/>
+                   """ % self.picture.url
+        else:
+            return None
 
     ViewPicture.allow_tags = True
+
+
+class Press(models.Model):
+
+    "Press model."
+
+    quoute = models.CharField(max_length=255, null=True, blank=True)
+    press_namme = models.CharField(max_length=255, null=True, blank=True)
+    image = models.ImageField(upload_to='press', null=True, blank=True)
+    link = models.URLField(null=True, blank=True)
+
+    def __unicode__(self):
+        """Return the press name"""
+        return self.press_namme
+
+    def showImage(self):
+        """Return a HTML with the press picture."""
+        if self.image.url:
+            return """<img src="%s" style="
+                      display: block; width: 50px;"/>
+                   """ % self.image.url
+        else:
+            return None
